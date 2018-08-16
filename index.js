b71e0c41969320d6ba455cd139cba4bf73b2e32a
taryn yaeger <tsyaeger@gmail.com>
2018-08-16 22:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,5 @@ const driver = 'bob';
 
 console.log("this code was called")
 
+console.log('driver')
 
-
-    it('invokes console.log() with the `driver` variable', function () {
-      expect(spy.calledWithExactly('bob')).to.be.true;
-    });
-
-    it('invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument', function () {
-      expect(spy.calledWithExactly('bob', 'is the driver variable name')).to.be.true;
-    });
-  });
